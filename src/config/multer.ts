f5af08d7c791a351476e26a85888840c773be9ca
ruth <ruthanastacio0@gmail.com>
2026-05-06 00:35:34 -0300

    add string no end point para render

--- src/config/multer.ts
+++ src/config/multer.ts
@@ -3,24 +3,21 @@ import multerS3 from 'multer-s3';
 import { S3Client } from '@aws-sdk/client-s3';
 import path from 'path';
 
-// 1. Configurando o que vai ligar para o MinIO / AWS S3
 const s3Config = new S3Client({
   region: process.env.AWS_REGION || 'us-east-1',
   credentials: {
     accessKeyId: process.env.AWS_ACCESS_KEY_ID as string,
     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY as string,
   },
-  endpoint: process.env.AWS_S3_ENDPOINT, 
+  endpoint: process.env.AWS_S3_ENDPOINT as string, 
   forcePathStyle: true, 
 });
 
-// 2. Configurando a "Gaveta" do Multer para salvar na nuvem
 export const upload = multer({
   storage: multerS3({
     s3: s3Config,
     bucket: process.env.AWS_S3_BUCKET as string,
     
-    // 👇 AQUI ESTÁ A CORREÇÃO: Adicionamos ": any" nos parâmetros
     key: function (req: any, file: any, cb: any) {
       const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9);
       const extension = path.extname(file.originalname);
